d1512810db90aa2bd955df1ba02edbe5c9017d6c
Thuc Nguyen <36192921+thucldnguyen@users.noreply.github.com>
2025-06-22 14:28:07 +0700

    updated GA tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,9 +74,28 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: settings.ga,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          settings.ga, // Google Analytics / GA
+          settings.ga4, // Google Analytics 4
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     {
